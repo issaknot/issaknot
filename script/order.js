a413241b131b0a1b8ae591586633a282412001c6
Zuhlek <tristan_kindle@gmx.com>
2023-01-23 08:39:48 +0100

    test fetch api

---
+++ script/order.js
@@ -0,0 +1,44 @@
+var selectedForm = null;
+var selectedColor = null;
+
+function selectForm(image) {
+  if(selectedForm!=null){ 
+    selectedForm.classList.remove("selected");
+  }
+  selectedForm = image;
+  selectedForm.classList.add("selected");
+}
+
+function selectColor(image) {
+  if(selectedColor!=null){ 
+    selectedColor.classList.remove("selected");
+  }
+  selectedColor = image;
+  selectedColor.classList.add("selected");
+}
+
+function submitOrder() {
+  // code to handle order submission goes here
+}
+
+async function load_available_colors() {
+    // Use the fetch API to get a list of files in the "images/colors" folder
+    const response = await fetch('https://issaknot.github.io/images/colors/');
+    const data = await response.json();
+    const imageList = data.filter(item => item.endsWith('.png'));
+
+    // Iterate through the list of images and create <img> elements
+    for (const image of imageList) {
+      const imgElement = document.createElement('img');
+      imgElement.src = `https://issaknot.github.io/images/colors/${image}`;
+      imgElement.onclick = selectColor(image);
+      document.getElementById('available_colors').appendChild(imgElement);
+    }
+  }
+
+function selectColor(el) {
+// do something with selected image
+    console.log(el.src);
+}
+
+window.onload = load_available_colors();
