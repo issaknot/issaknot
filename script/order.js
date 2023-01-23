57f23f7912a7f9c9edf827e01c21cafbf166da32
Zuhlek <tristan_kindle@gmx.com>
2023-01-23 13:36:06 +0100

    added email lgoic

--- script/order.js
+++ script/order.js
@@ -3,42 +3,57 @@ var selectedColor = null;
 
 function selectForm(image) {
   if(selectedForm!=null){ 
-    selectedForm.classList.remove("selected");
+    selectedForm.classList.remove("selected_form");
   }
   selectedForm = image;
-  selectedForm.classList.add("selected");
+  selectedForm.classList.add("selected_form");
 }
 
 function selectColor(image) {
   if(selectedColor!=null){ 
-    selectedColor.classList.remove("selected");
+    selectedColor.classList.remove("selected_color");
   }
   selectedColor = image;
-  selectedColor.classList.add("selected");
+  selectedColor.classList.add("selected_color");
 }
 
-function submitOrder() {
-  // code to handle order submission goes here
+async function load_available_forms() {
+    const response = await fetch('https://api.github.com/repos/issaknot/shop/contents/images/forms');
+    const data = await response.json();
+    data.forEach(function(file) {
+        var img = document.createElement("img");
+        img.src = file.download_url;
+        img.onclick = selectForm.bind(null, img);
+        document.getElementById("available_forms").appendChild(img);
+      });
 }
 
 async function load_available_colors() {
-    // Use the fetch API to get a list of files in the "images/colors" folder
     const response = await fetch('https://api.github.com/repos/issaknot/shop/contents/images/colors');
     const data = await response.json();
-    const imageList = data.filter(item => item.endsWith('.png'));
-
-    // Iterate through the list of images and create <img> elements
-    for (const image of imageList) {
-      const imgElement = document.createElement('img');
-      imgElement.src = image.donwload_url;
-      imgElement.onclick = selectColor(image);
-      document.getElementById('available_colors').appendChild(imgElement);
-    }
-  }
-
-function selectColor(el) {
-// do something with selected image
-    console.log(el.src);
+    data.forEach(function(file) {
+        var img = document.createElement("img");
+        img.src = file.download_url;
+        img.onclick = selectColor.bind(null, img);
+        document.getElementById("available_colors").appendChild(img);
+        });
 }
 
-window.onload = load_available_colors();
+function add_submit_listener() {
+    let form = document.getElementById("order_form");
+    form.addEventListener("submit", function (event) {
+        let desiredform = document.getElementsByClassName('selected_form')[0].src.split("/").pop();
+        let desiredcolor = document.getElementsByClassName('selected_color')[0].src.split("/").pop();
+        let orderDetails = `The visitor selected form = ${desiredform} and color = ${desiredcolor}`;
+        document.getElementById("order_message").value = orderDetails;
+    });
+}
+
+function setupContent(){
+    load_available_colors()
+    load_available_forms()
+    add_submit_listener();
+}
+
+window.onload = setupContent();
+
